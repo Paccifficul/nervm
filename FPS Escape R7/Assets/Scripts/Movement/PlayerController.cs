9ec7f7b6b482eb27365841993f7d8bcbba692a95
Paccifficul <elizarov.dm1337@gmail.com>
2022-02-06 23:01:56 +0300

    add test map and update code
    ДОБАВЛЕНО:
    -Тестовая карта
    -Добавлен спринт
    -Добавлены комментарии к коду в PlayerController.cs

--- FPS Escape R7/Assets/Scripts/Movement/PlayerController.cs
+++ FPS Escape R7/Assets/Scripts/Movement/PlayerController.cs
@@ -5,39 +5,78 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Rigidbody))]
+/// <summary>
+/// Базовый класс управление игрока
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
-    // ==== VARIABLES ====
-    // ---- Speed ----
-    [SerializeField]
-    protected float speed = 3f;
+    // ==== ПОЛЯ КЛАССА ====
+    // ---- Скорость ----
+    /// <value>Скорость передвижения</value>
+    private readonly float walkSpeed = 4f;
+    /// <value>Скорость бега</value>
+    private readonly float sprintSpeed = 7f;
+    /// <value>Коэффициент ускорения</value>
     protected float speedCoef = 1f;
-    Vector3 finalVelocity = Vector3.zero;
+    /// <value>Финальная скорость</value>
+    private Vector3 finalVelocity = Vector3.zero;
 
-    // ---- Movement ----
-    private Vector3 horMove; // Go left or right
-    private Vector3 verMove; // Go forward or back
+    // ---- Передвижение ----
+    /// <value>Горизонтальная скорость</value>
+    private Vector3 horMove;
+    /// <value>Вертикальная скорость</value>
+    private Vector3 verMove;
 
-    // ---- Jump ----
+    // ---- Прыжок ----
     [SerializeField]
-    private float jumpForceCoef = 10f;
-    private bool isJumpButtonClamping = false;
+    /// <value>Сила прыжка</value>
+    private float jumpForceCoef = 15f;
 
-    // ---- Rotation ----
+    // ---- Поворот ----
     [SerializeField]
-    private float mouseVerSensivity = 3f;
+    /// <value>Вертикальная скорость мыши</value>
+    private float mouseVerSensivity = 4f;
+    /// <value>Горизонтальая скорость мыши</value>
     [SerializeField]
-    private float mouseHorSensivity = 3f;
-    private float horRot; // Horizontal rotation
-    private float verRot = 0; // Vertical rotation
+    private float mouseHorSensivity = 4f;
+    /// <value>Градус поворота по горизонтали</value>
+    private float horRot;
+    /// <value>Градус поворота по вертикали</value>
+    private float verRot = 0;
+    /// <value>Горизонтальны поворот</value>
     private Vector3 horRotation = Vector3.zero;
 
-    // ---- Unity components ----
+    // ---- Компоненты Unity ----
+    /// <value>Камера игрока</value>
     [SerializeField]
     private new Camera camera;
     private new Rigidbody rigidbody;
 
+    // ==== Свойства ====
+    /// <summary>
+    /// Получаем значение скорости ходьбы
+    /// </summary>
+    /// <returns>Возращает значение скорости ходьбы</returns>
+    public float WalkSpeed
+    {
+        get
+        {
+            return walkSpeed;
+        }
+    }
+    /// <summary>
+    /// Получаем значение скорости при беге
+    /// </summary>
+    /// /// <returns>Возращает значение скорости бега</returns>
+    public float SprintSpeed
+    {
+        get
+        {
+            return sprintSpeed;
+        }
+    }
 
+    // ==== МЕТОДЫ/ФУНКЦИИ (хз как правильно) ====
     // Start is called before the first frame update
     protected void Start()
     {
@@ -47,47 +86,62 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     protected void FixedUpdate()
     {
-        // Calculate speed
+        // Просчитываем скорость
         horMove = transform.right * Input.GetAxis("Horizontal");
         verMove = transform.forward * Input.GetAxis("Vertical");
 
-        finalVelocity = speed * speedCoef * (horMove + verMove).normalized;
+        finalVelocity = CalculateSpeed();
 
-        // Calculate rotate angle
+        // Просчитываем угол поворота
         horRot = Input.GetAxis("Mouse X");
         verRot += Input.GetAxis("Mouse Y") * mouseVerSensivity;
 
         horRotation = new Vector3(0f, horRot, 0f) * mouseHorSensivity;
 
-        MovePlayer(); // Apply move
-        ChangeViewAngle(); // Apply rotation
+        MovePlayer(); 
+        ChangeViewAngle();
     }
 
+    /// <summary>
+    /// Поворот игрока в сторону при помощи rigidbody.
+    /// Поворот камеры, с ограничением 90 градусов вверх и вниз
+    /// </summary>
     private void ChangeViewAngle()
     {
-        // Change view angle
         rigidbody.MoveRotation(rigidbody.rotation * Quaternion.Euler(horRotation));
 
-        // Rotate camera up/down with limit at -90 and 90 degress
         verRot = Mathf.Clamp(verRot, -90f, 90f);
         camera.transform.localEulerAngles = new Vector3(-verRot, 
             camera.transform.localEulerAngles.y, camera.transform.localEulerAngles.z);
     }
 
+    /// <summary>
+    /// Передвигаем игрока. Прыжок
+    /// </summary>
     private void MovePlayer()
     {
-        // Change position 
         rigidbody.MovePosition(rigidbody.position + finalVelocity * Time.fixedDeltaTime);
 
-        // Jump
-        if (IsGrounded() && Input.GetButton("Jump"))
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
             rigidbody.AddForce(Vector3.up * jumpForceCoef, ForceMode.Impulse);
-        print(IsGrounded().ToString() + ' ' + isJumpButtonClamping.ToString());
     }
 
-    // Check, if we're on ground
+    /// <summary>
+    /// Проверяем, находится ли игрок на земле или нет
+    /// </summary>
+    /// <returns>Стоит ли игрок на поверхности</returns>
     private bool IsGrounded()
     {
         return Physics.Raycast(transform.position, Vector3.down, 1.0f);
+    }
+
+    /// <summary>
+    /// Просчитываем скорость, учитывая ускорение 
+    /// </summary>
+    /// <returns>Фильнаная скорость игрока, проверяя, бежит ли он или идёт</returns>
+    private Vector3 CalculateSpeed()
+    {
+        if (Input.GetButton("Sprint")) return sprintSpeed * speedCoef * (horMove + verMove).normalized;
+        return walkSpeed * speedCoef * (horMove + verMove).normalized;
     }
 }
